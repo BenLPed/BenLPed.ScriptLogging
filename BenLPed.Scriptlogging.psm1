f0864265a1707847b7b6aa00228d66803d79814e
benlped <benni.l.pedersen@gmail.com>
2024-03-20 12:10:12 +0100

    Bug Fix

--- BenLPed.Scriptlogging.psm1
+++ BenLPed.Scriptlogging.psm1
@@ -2,3 +2,12 @@
 .$psScriptRoot\Public\start-log.ps1
 
 .$psScriptRoot\Public\Write-log.ps1
+
+
+
+. "$psScriptRoot\Public\Start-Log.ps1"
+
+. "$psScriptRoot\Public\Write-Log.ps1"
+
+
+Export-ModuleMember -Function 'Start-Log', 'Write-Log'
