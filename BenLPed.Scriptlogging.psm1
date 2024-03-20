69985fd087832e3884845f351b5fba90f75ba6f5
benlped <benni.l.pedersen@gmail.com>
2024-03-20 13:31:54 +0100

    Update

--- BenLPed.Scriptlogging.psm1
+++ BenLPed.Scriptlogging.psm1
@@ -3,11 +3,8 @@
 
 .$psScriptRoot\Public\Write-log.ps1
 
+# . "$psScriptRoot\Public\Start-Log.ps1"
 
+# . "$psScriptRoot\Public\Write-Log.ps1"
 
-. "$psScriptRoot\Public\Start-Log.ps1"
-
-. "$psScriptRoot\Public\Write-Log.ps1"
-
-
-Export-ModuleMember -Function 'Start-Log', 'Write-Log'
+# Export-ModuleMember -Function 'Start-Log', 'Write-Log'
